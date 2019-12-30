d0aa24d4b181bcfae58ff24069334549fe5a892c
Aline L <aline.lanckneus@live.be>
2019-12-30 17:01:32 +0100

    table

--- mysql/connection.php
+++ mysql/connection.php
@@ -1,20 +1,44 @@
 <?php
 
-    function openConnection() {
+    //function openConnection() {
+        
+            $dbhost     = "localhost";
+            $dbuser     = "test_user";
+            $dbpass     = "123";
+            $db         = "becode";
 
-        $dbhost     = "localhost";
-        $dbuser     = "test_user";
-        $dbpass     = "123";
-        $db         = "becode";
+        try { 
+            $pdo = new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-        $pdo = new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        /* $sql = "INSERT INTO student (first_name, last_name, email) VALUES ('Peter', 'Parker', 'peterparker@mail.com')";    
+        $pdo->exec($sql);
+            echo "New record created successfully";
+            }
+        catch(PDOException $e)
+            {
+            echo $sql . "<br>" . $e->getMessage();
+            }
 
-        return $pdo;
+            unset($pdo); */
 
+        $sql = "INSERT INTO student (first_name, last_name, username, gender, email, created_at, pref_language) VALUES (:first_name, :last_name, :username, :gender, :email, :created_at, :pref_language)";
+        $stmt = $pdo->prepare($sql);
 
-    }
+        $stmt->bindParam(':first_name', $_REQUEST['first_name']);
+        $stmt->bindParam(':last_name', $_REQUEST['last_name']);
+        $stmt->bindParam(':username', $_REQUEST['username']);
+        $stmt->bindParam(':gender', $_REQUEST['gender']);
+        $stmt->bindParam(':email', $_REQUEST['email']);
+        $stmt->bindParam(':created_at', $_REQUEST['created_at']);
+        $stmt->bindParam(':pref_language', $_REQUEST['pref_language']);
 
+        $stmt->execute();
+        echo 'records inserted successfully';
+        } catch(PDOException $e){
+            die("error: could not execute $sql " . $e->getMessage());
+        }
+        unset($pdo);
 
 ?>
--- mysql/insert.php
+++ mysql/insert.php
@@ -1,3 +1,57 @@
 <?php
     include('./connection.php');
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="stylesheet" href="css/styles.css">
+<title>Add Record Form</title>
+</head>
+<body>
+    <form action="insert.php" method="post">
+        <p>
+            <label for="firstName">First Name:</label>
+            <input type="text" name="first_name" id="firstName">
+        </p>
+        <p>
+            <label for="lastName">Last Name:</label>
+            <input type="text" name="last_name" id="lastName">
+        </p>
+        <p>
+            <label for="userName">Username:</label>
+            <input type="text" name="username" id="userName">
+        </p>
+        <p>
+            <label for="gender">Gender:</label><br>
+            <input type="radio" name="gender" id="gender" value="female">Female<br>
+            <input type="radio" name="gender" id="gender" value="male">Male<br>
+            <input type="radio" name="gender" id="gender" value="non-binary">Non-binary<br>
+            
+            
+        </p>
+        <p>
+            <label for="emailAddress">Email Address:</label>
+            <input type="text" name="email" id="emailAddress">
+        </p>
+        <p>
+            <label for="created_at">Created At:</label>
+            <input type="date" name="created_at" id="created_at" value="<?php echo date("Y-m-d"); ?>">
+        </p>
+        <p>
+            <label for="preferred_language">Preferred Language:</label>
+            <input type="text" name="pref_language" id="language">
+        </p>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+

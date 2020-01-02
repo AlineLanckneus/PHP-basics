ccd3ec63a6b858555817451601aec1486e87d33f
Aline L <aline.lanckneus@live.be>
2020-01-02 16:58:49 +0100

    profile page works but ugly af

--- mysql/profile.php
+++ mysql/profile.php
@@ -1,28 +1,27 @@
 <?php
     require('connection.php');
     //require('insert.php');
-    session_start();
+    
+    //session_start();
     echo var_dump($_GET);
     $pdo = getPdo();
     $userId = $_GET['user'];
     $fname = $lname = $username = $gender = $email = $preflang = "";
     /* $userId = isset($_GET['id']) ? $_GET['id'] : "" ; */
-    var_dump($_GET['id']);
+    //var_dump($_GET['id']);
     $result2 = $pdo->query("SELECT * FROM NewTable WHERE id = '$userId'");
 
         while($row2 = $result2->fetch(PDO::FETCH_ASSOC))
         {
             $userId = $row2['id'];
-            $fname = $row2['first_name'];
-            $lname = $row2['last_name'];
+            $fname = ucwords($row2['first_name']);
+            $lname = ucwords($row2['last_name']);
             $username = $row2['username'];
-            $gender = $row2['gender'];
+            $gender = ucwords($row2['gender']);
             $email = $row2['email'];
-            $preflang = $row2['pref_language'];
+            $preflang = strtoupper($row2['pref_language']);
         }
-//echo 'Session: <br>' . var_dump($_SESSION);
-echo 'GET: ' . var_dump($_GET);
-echo 'POST: ' . var_dump($_POST);
+
 ?>
 
 <!DOCTYPE html>
@@ -31,43 +30,57 @@ echo 'POST: ' . var_dump($_POST);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <title>Profile Page</title>
 </head>
-<body>
+<body class="profilePage">
     <?php
-        echo $fname;
-        echo $lname;
-        echo $username;
-        echo $gender;
-        echo $email;
+        echo $fname . '<br>';
+        echo $lname . '<br>';
+        echo $username . '<br>';
+        echo $gender . '<br>';
+        echo $email . '<br>';
+        echo $preflang . '<br>';
     ?>
-    <table width="398" border="0" align="center" cellpadding="0">
-  <tr>
-    <td height="26" colspan="2">Your Profile Information </td>
-	<td><div align="right"><a href="index.php">logout</a></div></td>
-  </tr>
-  <tr>
-    <td width="129" rowspan="5"><img src="<?php echo $picture ?>" width="129" height="129" alt="no image found"/></td>
-    <td width="82" valign="top"><div align="left">FirstName:</div></td>
-    <td width="165" valign="top"><?php echo $fname ?></td>
-  </tr>
-  <tr>
-    <td valign="top"><div align="left">LastName:</div></td>
-    <td valign="top"><?php echo $lname ?></td>
-  </tr>
-  <tr>
-    <td valign="top"><div align="left">Gender:</div></td>
-    <td valign="top"><?php echo $gender ?></td>
-  </tr>
-  <tr>
-    <td valign="top"><div align="left">Address:</div></td>
-    <td valign="top"><?php echo $address ?></td>
-  </tr>
-  <tr>
-    <td valign="top"><div align="left">Contact No.: </div></td>
-    <td valign="top"><?php echo $contact ?></td>
-  </tr>
-</table>
-<p align="center"><a href="index.php"></a></p>
+    <!-- <div class="container"> 
+        <img class="meme" src="https://belikebill.ga/billgen-API.php?default=1" />
+    </div> -->
+    
+    <div class="table-responsive"> 
+        <table class="table-dark">
+            <thead> 
+                <tr>
+                    <th>Your Profile Information </th>
+                </tr>
+            </thead>
+            <tbody> 
+                <tr>
+                    <td><div align="left">FirstName:</div></td>
+                    <td ><?php echo $fname ?></td>
+                </tr>
+                <tr>
+                    <td ><div align="left">LastName:</div></td>
+                    <td ><?php echo $lname ?></td>
+                </tr>
+                <tr>
+                    <td ><div align="left">Username:</div></td>
+                    <td><?php echo $username ?></td>
+                </tr>
+                <tr>
+                    <td ><div align="left">Gender:</div></td>
+                    <td ><?php echo $gender ?></td>
+                </tr>
+                <tr>
+                    <td ><div align="left">Email: </div></td>
+                    <td ><?php echo $email ?></td>
+                </tr>
+                <tr>
+                    <td ><div align="left">Preferred Language: </div></td>
+                    <td ><?php echo $preflang ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
